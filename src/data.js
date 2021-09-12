95e6ce8077f3197443b8d8b1096f3d1a995c0b30
Jack Peplinski <30737750+jackpeplinski@users.noreply.github.com>
2021-09-12 18:00:39 -0400

    Add more data

--- src/data.js
+++ src/data.js
@@ -1,44 +1,224 @@
 export const data = [
     {
-        "company": "Autodesk",
+        "company": "Amazon",
         "status": "Open",
+        "pastOpening": "September",
+        "location": "Calgary, Toronto, Vancouver"
+    },
+    {
+        "company": "Google",
+        "status": "Not Open",
         "pastOpening": "September",
         "location": "Calgary"
     },
     {
-        "company": "Autodesk",
+        "company": "RBC",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "TopHat",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "CIBC",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "OTPP",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "Capital One",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "Publicis Sapient",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "Splunk",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "Mastercard",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "Microsoft",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "Nvidia",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "The Score",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "CSE",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "Pinterest",
         "status": "Open",
         "pastOpening": "September",
-        "location": "Calgary"
+        "location": "Toronto"
+    },
+    {
+        "company": "Sunlife",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "Mark43",
+        "status": "Not Open",
+        "pastOpening": "September",
+        "location": "Toronto"
+    },
+    {
+        "company": "Unity",
+        "status": "Not Open",
+        "pastOpening": "December",
+        "location": "Montreal"
+    },
+    {
+        "company": "IBM",
+        "status": "Not Open",
+        "pastOpening": "December",
+        "location": "Toronto"
+    },
+    {
+        "company": "BMO",
+        "status": "Not Open",
+        "pastOpening": "December",
+        "location": "Toronto"
+    },
+    {
+        "company": "Square",
+        "status": "Not Open",
+        "pastOpening": "December",
+        "location": "Waterloo"
+    },
+    {
+        "company": "Zynga",
+        "status": "Not Open",
+        "pastOpening": "January",
+        "location": "Toronto"
+    },
+    {
+        "company": "Shopify",
+        "status": "Not Open",
+        "pastOpening": "January",
+        "location": "Remote"
+    },
+    {
+        "company": "Pagerduty",
+        "status": "Not Open",
+        "pastOpening": "January",
+        "location": "Toronto"
+    },
+    {
+        "company": "Bolt",
+        "status": "Not Open",
+        "pastOpening": "January",
+        "location": "Toronto"
+    },
+    {
+        "company": "Intact",
+        "status": "Not Open",
+        "pastOpening": "January",
+        "location": "Toronto"
+    },
+    {
+        "company": "Geotab",
+        "status": "Not Open",
+        "pastOpening": "January",
+        "location": "Toronto"
     },
     {
         "company": "Autodesk",
-        "status": "Open",
-        "pastOpening": "September",
-        "location": "Calgary"
+        "status": "Not Open",
+        "pastOpening": "January",
+        "location": "Toronto, Montreal"
     },
     {
-        "company": "Autodesk",
-        "status": "Open",
-        "pastOpening": "September",
-        "location": "Calgary"
+        "company": "Nanoleaf",
+        "status": "Not Open",
+        "pastOpening": "February",
+        "location": "Toronto"
     },
     {
-        "company": "Autodesk",
-        "status": "Open",
-        "pastOpening": "September",
-        "location": "Calgary"
+        "company": "State Street",
+        "status": "Not Open",
+        "pastOpening": "February",
+        "location": "Toronto"
     },
     {
-        "company": "Autodesk",
-        "status": "Open",
-        "pastOpening": "September",
-        "location": "Calgary"
+        "company": "Interac",
+        "status": "Not Open",
+        "pastOpening": "February",
+        "location": "Toronto"
     },
     {
-        "company": "Autodesk",
-        "status": "Open",
-        "pastOpening": "September",
-        "location": "Calgary"
-    }
+        "company": "Loblaw Digital",
+        "status": "Not Open",
+        "pastOpening": "February",
+        "location": "Toronto"
+    },
+    {
+        "company": "Wish",
+        "status": "Not Open",
+        "pastOpening": "February",
+        "location": "Toronto"
+    },
+    {
+        "company": "WealthSimple",
+        "status": "Not Open",
+        "pastOpening": "February",
+        "location": "Toronto"
+    },
+    {
+        "company": "Telus Digital",
+        "status": "Not Open",
+        "pastOpening": "February",
+        "location": "Toronto"
+    },
+    {
+        "company": "Vena Solutions",
+        "status": "Not Open",
+        "pastOpening": "February",
+        "location": "Toronto"
+    },
+    {
+        "company": "Aviva",
+        "status": "Not Open",
+        "pastOpening": "February",
+        "location": "Toronto"
+    },
 ]
