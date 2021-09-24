15bf63c5122d2fdd51d1d80450d056548875b47c
Jack Peplinski <30737750+jackpeplinski@users.noreply.github.com>
2021-09-24 09:31:44 -0400

    Favicon attempt again

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,10 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Canadian Software Intern",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: ["gatsby-plugin-styled-components", {
+    resolve:"gatsby-plugin-sharp",
+    options: {
+      icon: "src/images/gatsby-icon.png"
+    }
+  }],
 };
